394b236c5055b2ef96efd1aaa8a2a4aa49c1547f
Benedict Ide <benedictide@gmail.com>
2023-04-29 14:15:03 -0400

    Initial commit

---
+++ assets/jsdata/encounter01.js
@@ -0,0 +1,43 @@
+let id = "e01";
+var encounters = encounters || {}
+encounters[id] = {
+    id:id,
+    desc:"A chance encounter...",
+    images:{
+        "normal":"assets/img/mockup/encountery.png",
+        "changed":"assets/img/mockup/problems.png"
+    },
+    frames:[
+        {
+            image:"normal",
+            text:"This is some encounter text. It's just the first part of the encounter, though. Nothing all that unusual is happening yet. Press the continue button to proceed through the event.",
+            options: [
+                {text: "Continue",effect:"none",frameIdx:1}
+            ]
+        },
+        {
+            image:"changed",
+            text:"Oh dear! Something has happened in the encounter! It's time to make some sort of choice related to the events going on. Press one of the buttons on the right to proceed.",
+            options:[
+                {text: "Ignore the problem",effect:"none",frameIdx:2},
+                {text: "Try and stop the problem",effect:"none",frameIdx:3}
+            ]
+        },
+        {
+            image:"changed",
+            text:"You ignore what's going on here and proceed on your way. Press continue to end the event.",
+            options:[
+                {text: "Continue",effect:"end"}
+            ]
+        },
+        {
+            image:"normal",
+            text:"You fix the problem, so the problem isn't there anymore. It probably cost you some resources?? Press continue to end the event.",
+            options:[
+                {text: "Continue",effect:"end"}
+            ]
+        }
+    ]
+};
+encounters.ids = encounters.ids || [];
+encounters.ids.push(id);
---
+++ game.js
@@ -0,0 +1,172 @@
+var encounters = encounters || {}
+var stage;
+var assets = {};
+var g = {};
+
+//create default manifest
+var manifest = [
+    {id:"bg",src:"assets/img/mockup/woodsbg.png"},
+    {id:"overlay",src:"assets/img/ui/frame.png"},
+    {id:"seed",src:"assets/img/ui/seed_base.png"},
+    {id:"char",src:"assets/img/mockup/char.png"},
+    {id:"moteslot",src:"assets/img/ui/mote_sidebar.png"},
+    {id:"motedot",src:"assets/img/ui/mote_dot.png"},
+    {id:"BOND",src:"assets/img/ui/mote_bond.png"},
+    {id:"HEAT",src:"assets/img/ui/mote_heat.png"},
+    {id:"COLD",src:"assets/img/ui/mote_cold.png"},
+    {id:"LIGHT",src:"assets/img/ui/mote_light.png"},
+    {id:"EARTH",src:"assets/img/ui/mote_earth.png"},
+    {id:"AIR",src:"assets/img/ui/mote_air.png"}
+]
+//populate manifest from encounter data
+for(let i = 0; i < encounters.ids.length; i++){
+    let encid = encounters.ids[i];
+    let enc = encounters[encid];
+    for (const [key, value] of Object.entries(enc.images)) {
+        let item = {id:encid + key,src:value};
+        manifest.push(item);
+    }
+}
+
+function initializeGame(){
+    var loadQueue = new createjs.LoadQueue(false);
+    loadQueue.on("fileload",onFileLoad,this);
+    loadQueue.on("complete",onFilesComplete,this);
+    loadQueue.loadManifest(manifest);
+}
+function tryInit(){
+    stage = new createjs.Stage("maincanv");
+    g.bg = new createjs.Bitmap(assets.bg);
+    g.bg.x = 0;
+    g.bg.y = 0;
+    g.overlay = new createjs.Bitmap(assets.overlay);
+    g.overlay.x = 0;
+    g.overlay.y = 0;
+    g.seed = new createjs.Bitmap(assets.seed);
+    g.seed.x = 27;
+    g.seed.y = 4;
+    g.char = new createjs.Bitmap(assets.char);
+    g.char.x = 685;
+    g.char.y = 222;
+    stage.addChild(g.bg);
+    stage.addChild(g.char);
+    stage.addChild(g.overlay);
+    stage.addChild(g.seed);
+
+    g.motes = [
+        {id:"BOND",count: 1 + Math.floor(Math.random() * 9)},
+        {id:"HEAT",count: Math.floor(Math.random() * 10)},
+        {id:"COLD",count: Math.floor(Math.random() * 10)},
+        {id:"LIGHT",count: Math.floor(Math.random() * 10)},
+        {id:"EARTH",count: Math.floor(Math.random() * 10)},
+        {id:"AIR",count: Math.floor(Math.random() * 10)}
+    ]
+    for(let i = 0; i < g.motes.length; i++){
+        let moteContainer = new createjs.Container();
+        let slot = new createjs.Bitmap(assets.moteslot);
+        slot.x = 0;
+        slot.y = 0;
+        moteContainer.addChild(slot);
+        let icon = new createjs.Bitmap(assets[g.motes[i].id]);
+        icon.x = 5;
+        icon.y = 3;
+        moteContainer.addChild(icon);
+        let dotBox = new createjs.Container();
+        dotBox.x = 50;
+        dotBox.y = 14;
+        dotBox.name = "dots";
+        moteContainer.addChild(dotBox);
+        moteContainer.x = 10;
+        moteContainer.y = 136 + (55*i);
+        stage.addChild(moteContainer);
+        g.motes[i].container = moteContainer;
+        updateMoteCount(i);
+    }
+
+    g.options = [];
+    g.encid = "e01";
+    loadEventFrame(g.encid);
+    stage.update();
+}
+function updateMoteCount(idx){
+    let mote = g.motes[idx];
+    let dotBox = mote.container.getChildByName("dots");
+    dotBox.removeAllChildren();
+    for(let i = 0; i < mote.count; i++){
+        let dot = new createjs.Bitmap(assets.motedot);
+        dot.x = i*6;
+        dot.y = (i%2 == 1 ? 8 : 0);
+        console.log("x and y are " + dot.x + " and " + dot.y);
+        dotBox.addChild(dot);
+    }
+    stage.update();
+}
+function loadEventFrame(encid, idx = 0){
+    unloadEventFrame();
+    if(encid == null) return;
+    g.encid = encid;
+    let enc = encounters[encid];
+    let first = enc.frames[idx];
+    let options = first.options;
+    g.encounterText = new createjs.Text(first.text,"14px Arial","#FFFFFF");
+    g.encounterText.lineWidth = 520;
+    g.encounterText.lineHeight = 22;
+    g.encounterImg = new createjs.Bitmap(assets[enc.id + first.image]);
+    g.encounterImg.x = 140;
+    g.encounterImg.y = 110;
+    g.encounterText.x = 140;
+    g.encounterText.y = 265;
+    stage.addChild(g.encounterImg);
+    stage.addChild(g.encounterText);
+    setOptions(options);
+    stage.update();
+}
+function unloadEventFrame(){
+    stage.removeChild(g.encounterImg);
+    stage.removeChild(g.encounterText);
+    for(let i = 0; i < g.options.length; i++){
+        stage.removeChild(g.options[i]);
+    }
+    stage.update();
+}
+function setOptions(options){
+    g.options = [];
+    for(let i = 0; i < options.length; i++){
+        let opt = options[i];
+        let optObj = new createjs.Container();
+        let optText = new createjs.Text(opt.text,"16px Arial","#FFFFFF");
+        optText.x = 8;
+        optText.y = 8;
+        let width = optText.getBounds().width;
+        let buttonbg = new createjs.Shape();
+        buttonbg.graphics.beginFill("#000000").drawRoundRect(0,0,width+18,30,9);
+        optObj.addChild(buttonbg);
+        optObj.addChild(optText);
+        optObj.x = 677;
+        optObj.y = 115 + (i*40);
+        //add button logic
+        optObj.addEventListener("click",function(event){
+            if(opt.frameIdx){
+                loadEventFrame(g.encid,opt.frameIdx);
+            }else if(opt.effect == "end"){
+                unloadEventFrame();
+                g.encid = null;
+            }else{
+                console.log(opt.effect);
+            }
+        })
+
+        g.options.push(optObj);
+        stage.addChild(optObj);
+    }
+    stage.update();
+}
+function onFileLoad(event){
+    assets[event.item.id] = event.result;
+}
+function onFilesComplete(event){
+    tryInit();
+}
+function loadTestEvent(){
+    loadEventFrame("e01");
+}
